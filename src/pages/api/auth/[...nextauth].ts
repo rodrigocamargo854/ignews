c1e5a0cbe26d98ae9e83894c8beb02aff4c50421
rodrigo <rodrigocamargo854@gmail.com>
2022-06-19 08:28:10 -0300

    fixed read me and inserted configuration for fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,25 @@
-import console from 'console';
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers'
+import console from "console";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 //get using fauna to save all users logged on application
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
-export default  NextAuth ({
+export default NextAuth({
+  //scope is all necessary information
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      //queries for fauna
+      const { email } = user;
 
-    //scope is all necessary information
-    providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-    ],
-    callbacks: {
-        async signIn(user,account,profile){
-
-            return true
-        },
-    }
-})
+      await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      return true;
+    },
+  },
+});
