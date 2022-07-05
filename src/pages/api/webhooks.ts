a10f32ed805b53a819658a72ac09fe455faa1a53
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-07-05 08:47:25 -0300

    web hooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,6 +1,43 @@
-import {NextApiRequest,NextApiResponse} from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import { Stripe } from "stripe";
 
-export default function ( req: NextApiRequest, res: NextApiResponse){
-    console.log('evento recebido')
-    res.status(200).json({ok:true})
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
 }
+
+export const config = {
+  api: {
+    bodyPArser: false,
+  },
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const secret = req.headers["stripe-signature"];
+
+    let event: Stripe.Event;
+
+    try {
+      event = Stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (e) {
+      return res.status(400).send(`webhook error:${e.message}`);
+    }
+
+    res.status(200).json({ ok: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
+  }
+};
